8c9f649d176fffbcace9f13558d66a28fa08b820
Franz Ibañez <dryst0@users.noreply.github.com>
2026-02-10 19:16:39 +0100

    refactor(test): fix Testcontainers not cleaning up on macOS Docker Desktop
    Ryuk fails to detect JVM disconnection, causing postgres containers
    to pile up indefinitely.

--- src/test/java/com/jfi/api/employee/adapter/out/persistence/EmployeeRepositoryIntegrationTests.java
+++ src/test/java/com/jfi/api/employee/adapter/out/persistence/EmployeeRepositoryIntegrationTests.java
@@ -1,6 +1,5 @@
 package com.jfi.api.employee.adapter.out.persistence;
 
-import com.jfi.api.employee.domain.Employee;
 import com.jfi.api.employee.domain.EmployeeType;
 import com.jfi.api.employee.port.out.EmployeeRepository;
 import java.util.UUID;
@@ -8,20 +7,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
 import org.springframework.r2dbc.core.DatabaseClient;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import reactor.test.StepVerifier;
 
 @SpringBootTest
-@Testcontainers
+@Import(TestcontainersConfiguration.class)
 class EmployeeRepositoryIntegrationTests {
-
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17");
 
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -35,40 +27,51 @@ class EmployeeRepositoryIntegrationTests {
     @BeforeEach
     void setUp() {
         databaseClient.sql("DELETE FROM employee").then().block();
-        databaseClient.sql("INSERT INTO employee (uuid, first_name, last_name, employee_type) VALUES (:uuid, :firstName, :lastName, :employeeType)")
-                .bind("uuid", workerUuid)
-                .bind("firstName", "Juan")
-                .bind("lastName", "dela Cruz")
-                .bind("employeeType", EmployeeType.WORKER.name())
-                .then().block();
-        databaseClient.sql("INSERT INTO employee (uuid, first_name, last_name, employee_type) VALUES (:uuid, :firstName, :lastName, :employeeType)")
-                .bind("uuid", managerUuid)
-                .bind("firstName", "Maria")
-                .bind("lastName", "Santos")
-                .bind("employeeType", EmployeeType.MANAGER.name())
-                .then().block();
+        databaseClient
+            .sql(
+                "INSERT INTO employee (uuid, first_name, last_name, employee_type) VALUES (:uuid, :firstName, :lastName, :employeeType)"
+            )
+            .bind("uuid", workerUuid)
+            .bind("firstName", "Juan")
+            .bind("lastName", "dela Cruz")
+            .bind("employeeType", EmployeeType.WORKER.name())
+            .then()
+            .block();
+        databaseClient
+            .sql(
+                "INSERT INTO employee (uuid, first_name, last_name, employee_type) VALUES (:uuid, :firstName, :lastName, :employeeType)"
+            )
+            .bind("uuid", managerUuid)
+            .bind("firstName", "Maria")
+            .bind("lastName", "Santos")
+            .bind("employeeType", EmployeeType.MANAGER.name())
+            .then()
+            .block();
     }
 
     @Test
     void givenEmployeesExist_whenGetEmployees_thenReturnsAll() {
         StepVerifier.create(employeeRepository.getEmployees())
-                .expectNextCount(2)
-                .verifyComplete();
+            .expectNextCount(2)
+            .verifyComplete();
     }
 
     @Test
     void givenEmployeeExists_whenGetEmployeeById_thenReturnsEmployee() {
         StepVerifier.create(employeeRepository.getEmployeeById(workerUuid))
-                .expectNextMatches(employee ->
-                        employee.getFirstName().equals("Juan")
-                                && employee.getLastName().equals("dela Cruz")
-                                && employee.getEmployeeType() == EmployeeType.WORKER)
-                .verifyComplete();
+            .expectNextMatches(
+                employee ->
+                    employee.getFirstName().equals("Juan") &&
+                    employee.getLastName().equals("dela Cruz") &&
+                    employee.getEmployeeType() == EmployeeType.WORKER
+            )
+            .verifyComplete();
     }
 
     @Test
     void givenNoEmployee_whenGetEmployeeById_thenReturnsEmpty() {
-        StepVerifier.create(employeeRepository.getEmployeeById(UUID.randomUUID()))
-                .verifyComplete();
+        StepVerifier.create(
+            employeeRepository.getEmployeeById(UUID.randomUUID())
+        ).verifyComplete();
     }
 }
